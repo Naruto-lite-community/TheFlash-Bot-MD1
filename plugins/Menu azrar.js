632a7bb8e75b47040fead9fe19bd9d354a8a946f
Mando90mando <Mandoelhakemmandooo@gmail.com>
2024-10-04 21:38:18 +0300

    Update Menu azrar.js

--- plugins/Menu azrar.js
+++ plugins/Menu azrar.js
@@ -24,7 +24,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     let readMore = more.repeat(850);
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-  await conn.sendMessage(m.chat, { react: { text: '📂', key: m.key } })
+  await conn.sendMessage(m.chat, { react: { text: '😝“', key: m.key } })
   const zack = 'https://envs.sh/wHc.jpg'
   const mentionId = m.key.participant || m.key.remoteJid;
  
